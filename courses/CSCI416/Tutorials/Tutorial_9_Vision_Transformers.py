1a596474ebcb305caff4b80cc6c72a2cdb166a75
Ashley Gao <gaoye0718@gmail.com>
2023-11-28 21:01:17 -0500

    WIP

--- courses/CSCI416/Tutorials/Tutorial_9_Vision_Transformers.py
+++ courses/CSCI416/Tutorials/Tutorial_9_Vision_Transformers.py
@@ -1,206 +1,123 @@
-import numpy as np
-import torch
 import torch.nn as nn
-from datasets import Array3D, ClassLabel, Features, load_dataset
-from matplotlib import pyplot
-from numpy import inf
-from sklearn.utils.class_weight import compute_class_weight
-from torchinfo import summary
-from tqdm import tqdm
-from transformers import AdamW, ViTFeatureExtractor, ViTModel
+import numpy as np
 
-# load cifar10
+# Import necessary modules and libraries
+from transformers import ViTFeatureExtractor, ViTModel, default_data_collator, EarlyStoppingCallback
+from transformers import ViTForImageClassification, TrainingArguments, Trainer
+from transformers.modeling_outputs import SequenceClassifierOutput
+from datasets import load_dataset, Image, load_metric, Features, ClassLabel, Array3D
+
+# Load the CIFAR-10 dataset and split it into training and test sets
 train_ds, test_ds = load_dataset('cifar10', split=['train[:5000]', 'test[:2000]'])
-# split up training into training + validation
+
+# Split the training set into training and validation subsets
 splits = train_ds.train_test_split(test_size=0.1)
 train_ds = splits['train']
 val_ds = splits['test']
-chk_img = train_ds[67]
-img = np.array(chk_img['img'])
-print("Shape: ", img.shape)
-cats = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-print("Animal: ", cats[chk_img['label']])
-pyplot.imshow(img, cmap=pyplot.get_cmap('gray'))
+# Create a ViT feature extractor
+feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
 
+# Define a data collator
+data_collator = default_data_collator
+
+# Define a function to preprocess images
 def preprocess_images(examples):
-    # get batch of images
     images = examples['img']
-    # convert to list of NumPy arrays of shape (C, H, W)
+
+    # Convert images to NumPy arrays
     images = [np.array(image, dtype=np.uint8) for image in images]
-    images = [np.moveaxis(image, source=-1, destination=0) for image    in images]
-    # preprocess and add pixel_values
+
+    # Move the channel dimension to the front (from HWC to CHW)
+    images = [np.moveaxis(image, source=-1, destination=0) for image in images]
+
+    # Extract pixel values using the feature extractor
     inputs = feature_extractor(images=images)
     examples['pixel_values'] = inputs['pixel_values']
+
     return examples
 
+# Define the dataset features
 features = Features({
     'label': ClassLabel(names=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']),
-    'img': Array3D(dtype="int64", shape=(3,32,32)),
+    'img': Image(decode=True, id=None),
     'pixel_values': Array3D(dtype="float32", shape=(3, 224, 224)),
 })
+
+# Apply preprocessing to training, validation, and test datasets
 preprocessed_train_ds = train_ds.map(preprocess_images, batched=True, features=features)
 preprocessed_val_ds = val_ds.map(preprocess_images, batched=True, features=features)
 preprocessed_test_ds = test_ds.map(preprocess_images, batched=True, features=features)
-# set format to PyTorch
-preprocessed_train_ds.set_format('torch', columns=['pixel_values', 'label'])
-preprocessed_val_ds.set_format('torch', columns=['pixel_values', 'label'])
-preprocessed_test_ds.set_format('torch', columns=['pixel_values', 'label'])
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# create dataloaders
-train_batch_size = 10
-eval_batch_size = 10
-train_dataloader = torch.utils.data.DataLoader(preprocessed_train_ds, batch_size=train_batch_size, shuffle=True, num_workers=2)
-val_dataloader = torch.utils.data.DataLoader(preprocessed_val_ds, batch_size=eval_batch_size, num_workers=2)
-test_dataloader = torch.utils.data.DataLoader(preprocessed_test_ds, batch_size=eval_batch_size, num_workers=2)
-batch = next(iter(train_dataloader))
+# Define training arguments
+args = TrainingArguments(
+    f"test-cifar-10",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=10,
+    per_device_eval_batch_size=4,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    load_best_model_at_end=False,
+    metric_for_best_model="accuracy",
+    logging_dir='logs',
+)
 
-assert batch['pixel_values'].shape == (train_batch_size, 3, 224, 224)
-assert batch['label'].shape == (train_batch_size,)
-
-vit_model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-
-for param in vit_model.parameters():
-      param.requires_grad = False
-
-class ViTForImageClassification(nn.Module):
+# Define a custom ViT model for image classification
+class ViTForImageClassification2(nn.Module):
     def __init__(self, num_labels=10):
-        super(ViTForImageClassification, self).__init__()
-        self.vit = vit_model
-        self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(self.vit.config.hidden_size,    num_labels)
+        super(ViTForImageClassification2, self).__init__()
+
+        # Load a pre-trained ViT model
+        self.vit = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+
+        # Add a linear classifier for the specified number of labels
+        self.classifier = nn.Linear(self.vit.config.hidden_size, num_labels)
         self.num_labels = num_labels
-    def forward(self, pixel_values):
+
+    def forward(self, pixel_values, labels):
+
+        # Forward pass through the ViT model
         outputs = self.vit(pixel_values=pixel_values)
-        output = self.dropout(outputs.last_hidden_state[:,0])
-        logits = self.classifier(output)
-        return logits
-model = ViTForImageClassification()
-model = model.to(device)
 
-#compute the class weights
-class_wts = compute_class_weight("balanced", np.unique(preprocessed_train_ds['label']), preprocessed_train_ds['label'].tolist())
-# print(class_wts)
-# # convert class weights to tensor
-weights= torch.tensor(class_wts,dtype=torch.float)
-weights = weights.to(device)
-# # loss function
-cross_entropy = nn.CrossEntropyLoss(weight=weights)
+        # Extract logits from the classifier
+        logits = self.classifier(outputs.last_hidden_state[:, 0])
 
-# define the optimizer
-optimizer = AdamW(model.parameters(), lr = 1e-3)
-# number of training epochs
-epochs = 50
+        loss = None
+        if labels is not None:
+            # Calculate the cross-entropy loss if labels are provided
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
 
-# function to train the model
-def train():
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
-    model.train()
-    total_loss = 0
-    # empty list to save model predictions
-    total_preds=[]
-    # iterate over batches
-    for step,batch in enumerate(train_dataloader):
-        # progress update after every 50 batches.
-        if step % 50 == 0 and not step == 0:
-            print('  Batch {:>5,}  of  {:>5,}.'.format(step,    len(train_dataloader)))
+# Initialize the custom ViT model
+model = ViTForImageClassification2()
 
-        # push the batch to gpu
-        lbl, pix = batch.items()
-        lbl, pix = lbl[1].to(device), pix[1].to(device)
+# Define a function to compute evaluation metrics
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return load_metric("accuracy").compute(predictions=predictions, references=labels)
 
-        # get model predictions for the current batch
-        preds = model(pix)
+# Initialize the Trainer for training and evaluation
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=preprocessed_train_ds,
+    eval_dataset=preprocessed_val_ds,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
 
-        # compute the loss between actual and predicted values
-        loss = cross_entropy(preds, lbl)
+# Train the model
+trainer.train()
 
-        # add on to the total loss
-        total_loss = total_loss + loss.item()
-
-        # backward pass to calculate the gradients
-        loss.backward()
-
-        # clip the the gradients to 1.0. It helps in preventing the    exploding gradient problem
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
-        # update parameters
-        optimizer.step()
-
-        # clear calculated gradients
-        optimizer.zero_grad()
-        preds=preds.detach().cpu().numpy()
-
-        # append the model predictions
-        total_preds.append(preds)
-    # compute the training loss of the epoch
-    avg_loss = total_loss / len(train_dataloader)
-
-    total_preds  = np.concatenate(total_preds, axis=0)
-
-    #returns the loss and predictions
-    return avg_loss, total_preds
-
-def eval():
-    total_loss = 0
-    model.eval() # prep model for evaluation
-    for step,batch in enumerate(val_dataloader):
-        lbl, pix = batch.items()
-        lbl, pix = lbl[1].to(device), pix[1].to(device)
-
-        # forward pass: compute predicted outputs by passing inputs to the model
-        preds = model(pix)
-        # calculate the loss
-        loss = cross_entropy(preds, lbl)
-        total_loss += loss.item()
-
-    return total_loss / len(val_dataloader)
-
-min_loss = inf
-es = 0
-for epoch in range(epochs):
-    print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
-
-    # Train model
-    train_loss, _ = train()
-    val_loss = eval()
-
-    # Early Stopping
-    if val_loss < min_loss:
-        min_loss = val_loss
-        es = 0
-    else:
-        es += 1
-        if es > 4:
-            print("Early stopping with train_loss: ", train_loss, "and val_loss for this epoch: ", val_loss, "...")
-            break
-
-    # it can make your experiment reproducible, similar to set  random seed to all options where there needs a random seed.
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    print(f'\n Training Loss: {train_loss:.3f}')
-    print(f'\n Validation Loss: {val_loss:.3f}')
-
-
-torch.save(model.state_dict(), '/working/model')
-
-def eval():
-    model.eval()
-    y_pred = []
-    y_true = []
-    with torch.no_grad():
-        for step, batch in tqdm(enumerate(test_dataloader), total = len(test_dataloader)):
-            lbl, pix = batch.items()
-            lbl, pix = lbl[1].to(device), pix[1].to(device)
-            outputs = model(pix)
-            outputs = torch.argmax(outputs, axis=1)
-            y_pred.extend(outputs.cpu().detach().numpy())
-            y_true.extend(lbl.cpu().detach().numpy())
-
-    return y_pred, y_true
-y_pred, y_true = eval()
-
-correct = np.array(y_pred) == np.array(y_true)
-accuracy = correct.sum() / len(correct)
-print("Accuracy of the model", accuracy)
+# Make predictions on the test dataset
+outputs = trainer.predict(preprocessed_test_ds)
+print(outputs)
